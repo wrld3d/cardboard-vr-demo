8eaf42c5c83bc56bf8d2a68bc92d0146cf12c62e
Aqif Hamid <aqifhamid@cap9008.netsolpk.com>
2016-05-31 17:58:36 +0500

    added apikey file again

---
+++ src/ApiKey.h
@@ -0,0 +1,13 @@
+// Copyright eeGeo Ltd (2012-2015), All Rights Reserved
+
+#pragma once
+
+#include <string>
+
+namespace
+{
+    
+    //! REQUIRED: You must obtain an API key for the eeGeo SDK from https://www.eegeo.com/developers/
+    static const std::string ApiKey = "3429dfad9362f1cd42cb614671e5199c";
+
+}
