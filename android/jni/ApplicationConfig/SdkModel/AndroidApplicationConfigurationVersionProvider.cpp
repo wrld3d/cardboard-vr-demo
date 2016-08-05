e62475695b2a6b3d664b79d068eeb129d6f6815b
Ali Arslan <aliarslan@Alis-MacBook-Pro.local>
2016-08-05 12:16:29 +0100

    #VR-134 #Version updated and minor config changes. #Buddy:Najhi

--- android/jni/ApplicationConfig/SdkModel/AndroidApplicationConfigurationVersionProvider.cpp
+++ android/jni/ApplicationConfig/SdkModel/AndroidApplicationConfigurationVersionProvider.cpp
@@ -2,6 +2,7 @@
 
 #include "AndroidApplicationConfigurationVersionProvider.h"
 #include "Types.h"
+#include <sstream>
 
 namespace Examples
 {
@@ -12,11 +13,16 @@ namespace Examples
 
         	AndroidApplicationConfigurationVersionProvider::AndroidApplicationConfigurationVersionProvider(const AndroidNativeState& nativeState)
             {
-
+        		// these are as as specified in AndroidManifest.xml
+        		// m_buildNumber <- android:versionCode
+        		// m_productVersion <- android:versionName
         		m_productVersion = nativeState.versionName;
+        		std::stringstream s;
+        		s << nativeState.versionCode;
+        		m_buildNumber = s.str();
 
             }
-            
+
             std::string AndroidApplicationConfigurationVersionProvider::GetProductVersionString() const
             {
                 return m_productVersion;
